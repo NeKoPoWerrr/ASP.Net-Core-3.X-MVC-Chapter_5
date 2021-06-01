9d148f502d26ede493206f7cb335d167a10d87ef
DESKTOP-40OA392\User <awp10520@gmail.com>
2021-06-02 02:39:46 +0800

    Startup

---
+++ Chapter_5/Controllers/FriendController.cs
@@ -0,0 +1,30 @@
+﻿using Chapter_5.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter_5.Controllers
+{
+    public class FriendController : Controller
+    {
+        private readonly DatabaseContext _context;
+        public FriendController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Friends.ToListAsync());
+        }
+        //GET:Friends/Details/5
+        /*public async Task<IActionResult> Details(int? id)
+        {
+            //return View(friend)
+        }
+        */
+    }
+}
---
+++ Chapter_5/Controllers/PassDataController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter_5.Controllers
+{
+    public class PassDataController : Controller
+    {
+        public IActionResult PetShop()
+        {
+            ViewData["Company"] = "汪星人寵物店"; //ViewData
+            ViewBag.Address = "台北市信義區"; //ViewBag
+            List<string> petList = new List<string>(); //List model
+            petList.Add("狗");
+            petList.Add("貓");
+            petList.Add("魚");
+            petList.Add("鼠");
+            petList.Add("變色龍");
+            ViewData.Model = petList;
+            return View();
+        }
+        public IActionResult PassViewData()
+        {
+            ViewData["Name"] = "Kenvin";
+            ViewData["Age"] = 33;
+            ViewData["Single"] = true;
+            //ViewData["Emplyees"] = empsList;
+            return View();
+        }
+    }
+}
