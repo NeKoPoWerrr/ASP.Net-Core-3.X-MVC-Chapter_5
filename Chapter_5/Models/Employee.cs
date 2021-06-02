78d173dbf68bb053226dbd74369607f909e33c8c
DESKTOP-40OA392\User <awp10520@gmail.com>
2021-06-02 20:04:37 +0800

    no message

--- Chapter_5/Models/Employee.cs
+++ Chapter_5/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace Chapter_5.Models
 {
     public class Employee
     {
+        [Display(Name = "員工編號")]
         public int Id { get; set; }
+        [Display(Name = "員工名稱")]
         public string Name { get; set; }
+        [Display(Name = "員工電話")]
         public string Phone { get; set; }
+        [Display(Name = "員工信箱")]
         public string Email { get; set; }
 
     }
